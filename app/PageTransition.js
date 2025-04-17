49e7c9be601e2fd3f4dc742be081af63a910ae74
Rectrix-21 <abhijithkrishnan14@gmail.com>
2025-04-16 21:55:41 -0600

    no_transition

--- app/PageTransition.js
+++ app/PageTransition.js
@@ -7,25 +7,28 @@ import { CSSTransition, SwitchTransition } from "react-transition-group";
 export default function PageTransition({ children }) {
   const pathname = usePathname();
   const [key, setKey] = useState(pathname);
-  const nodeRef = useRef(null);    // ← ref for CSSTransition
+  const nodeRef = useRef(null);
 
   useEffect(() => {
     setKey(pathname);
   }, [pathname]);
 
   return (
-    <SwitchTransition mode="out-in">
-      <CSSTransition
-        key={key}
-        nodeRef={nodeRef}          // ← pass ref here
-        timeout={300}
-        classNames="fade"
-        unmountOnExit
-      >
-        <div ref={nodeRef} className="page-transition">
-          {children}
-        </div>
-      </CSSTransition>
-    </SwitchTransition>
+    <div className="transition-wrapper">
+      <SwitchTransition mode="out-in">
+        <CSSTransition
+          key={key}
+          nodeRef={nodeRef}
+          timeout={300}
+          classNames="fade"
+          unmountOnExit
+          appear
+        >
+          <div ref={nodeRef} className="page-transition">
+            {children}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </div>
   );
 }
